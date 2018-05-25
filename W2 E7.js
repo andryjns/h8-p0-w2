7d0b5ca5bf681334e532a947597815046f37a761
andryjns <andry_jns@yahoo.com>
2018-05-25 12:05:23 +0700

    W2 Exercise 7
    [Exercises 7] Perulangan - Play with Asterisks

---
+++ W2 E7.js
@@ -0,0 +1,22 @@
+// TUGAS 1
+for (var rows1=1; rows1<=5; rows1++){
+    console.log('*')
+}
+
+
+// TUGAS 2
+for (var rows2=1; rows2<=5; rows2++){
+    console.log('*****')
+}
+
+
+// TUGAS  3
+var rows3=1
+var repeatstring=""
+
+while (rows3<=5){
+    repeatstring += "*"
+    console.log (repeatstring)
+    rows3++;
+}
+return repeatstring;
